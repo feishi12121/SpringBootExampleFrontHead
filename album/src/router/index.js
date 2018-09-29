3d919c34130755ca4b252a2488c5803081211c78
1094064939@qq.com <1094064939@qq.com>
2018-09-29 11:21:36 +0800

    add project album

---
+++ album/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Album from '@/components/Album'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Album',
+      component: Album
+    }
+  ]
+})
